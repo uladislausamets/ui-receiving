e7477e26c81d57717f1b0b871df0f32727dce150
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-01-14 16:59:15 +0200

    UIREC-34 Add success message for create title (#59)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## [1.0.0](https://github.com/folio-org/ui-receiving/tree/v1.0.0) (in progress)
 
 ### Stories
+* [UIREC-34](https://issues.folio.org/browse/UIREC-34) Add success message for create title
 * [UIREC-28](https://issues.folio.org/browse/UIREC-28) Add Title for receiving/check-in
 * [UIREC-27](https://issues.folio.org/browse/UIREC-27) View title in receiving area
 * [UIREC-26](https://issues.folio.org/browse/UIREC-26) Create three pane layout for receiving
--- src/TitleForm/TitleFormContainer.js
+++ src/TitleForm/TitleFormContainer.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import ReactRouterPropTypes from 'react-router-prop-types';
 
 import { stripesConnect } from '@folio/stripes/core';
-import { identifierTypesManifest } from '@folio/stripes-acq-components';
+import {
+  identifierTypesManifest,
+  useShowCallout,
+} from '@folio/stripes-acq-components';
 
 import { titlesResource } from '../common/resources';
 import TitleForm from './TitleForm';
@@ -11,6 +14,7 @@ import TitleForm from './TitleForm';
 function TitleFormContainer({ history, match, mutator }) {
   const titleId = match.params.id;
   const [identifierTypes, setIdentifierTypes] = useState();
+  const showCallout = useShowCallout();
 
   useEffect(() => {
     mutator.identifierTypes.GET()
@@ -24,15 +28,30 @@ function TitleFormContainer({ history, match, mutator }) {
     [history],
   );
   const onSubmit = useCallback(
-    // eslint-disable-next-line no-unused-vars
     ({ poLineNumber, ...newTitle }) => {
       return mutator.titles.POST(newTitle)
         .then(() => {
+          showCallout({
+            messageId: 'ui-receiving.title.actions.save.success',
+            type: 'success',
+            values: {
+              title: newTitle.title,
+              poLineNumber,
+            },
+          });
           setTimeout(() => history.push('/receiving'));
-        });
+        })
+        .catch(() => showCallout({
+          messageId: 'ui-receiving.title.actions.save.error',
+          type: 'error',
+          values: {
+            title: newTitle.title,
+            poLineNumber,
+          },
+        }));
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [history, titleId],
+    [history, showCallout, titleId],
   );
 
   if (!identifierTypes) {
